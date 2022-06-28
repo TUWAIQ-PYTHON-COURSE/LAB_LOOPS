12362b42113cb0bb0eb4bab02c11d657f49d97ed
fahad <fahadx1418@gmail.com>
2022-06-28 21:03:12 +0300

    done work

---
+++ main.py
@@ -0,0 +1,41 @@
+
+xrange = (45, 210)
+
+
+
+    
+
+for letter in range(45, 210):
+
+    #to exit a loop , we use break
+    if letter == 100:
+        continue
+
+    
+    elif letter == 201:
+        break
+
+    print(letter)
+
+
+
+
+x: str = input("what is the product of 7 * 24 ?     :")
+
+
+yy : int = 7
+nn : int = 24
+ 
+#xx = (yy*nn)
+
+
+while int(x) == xx :
+
+    print("You answered this Question correctly")
+
+else:
+    print("Your Answer is wrong try again..")
+
+
+#else:
+#print("Your Answer is wrong try again..")
