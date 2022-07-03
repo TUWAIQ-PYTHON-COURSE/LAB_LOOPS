bc264b284461a667504dbd800aceaf0a3a3a8301
Saad <saadfahad8011@gmail.com>
2022-07-03 05:06:41 +0300

    Done

---
+++ LAB_loop.py
@@ -0,0 +1,14 @@
+
+for x in range(45, 210, 1):
+    if x == 100:
+        continue
+    elif x == 205:          #  Question 1 
+        break
+    print(x)
+    
+while True:
+        s = int(input ("what is the product of 7 * 24 ?"))
+        if s == 7*24:
+            print("You answered this Question correctly")                  
+        else:                                                   # Question 2
+            print( "Your Answer is wrong try again..")
